61f633f31a8c3241c22158f1c778350a88f556f3
Eldritch Cheese <eldritch.cheese@gmail.com>
2021-10-12 07:03:38 -0500

    Updated example to PyQt5
    A little bit late, seeing how PyQt6 was released in January, but oh
    well.

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-Running "example.py" will open a GUI window demonstrating a few different inputs.
-The inputs are examples of dropdown menus,
-  a text entry field, an integer entry field,
-  and a floating point entry field.
-When the "Save" button is pressed,
-  it will ask the user for an output file location,
-  then save the current value of the inputs to that location.
+Packages
+--------
 
-The `example.ui` file was made using the `designer-qt4` program.
+To run, install the `PyQt5` package with `python3 -mpip install --user PyQt5`.
+
+To edit the `example.ui` file, first install the `pyqt5-tools` package
+with `python3 -mpip install --user pyqt5-tools`, then run as
+`pyqt5-tools designer`.
+
+Usage
+-----
+
+Running `example.py` will open a GUI window demonstrating a few
+different inputs.  The inputs are examples of dropdown menus, a text
+entry field, an integer entry field, and a floating point entry field.
+When the "Save" button is pressed, it will ask the user for an output
+file location, then save the current value of the inputs to that
+location.
--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt4 import uic, QtGui, QtCore
+from PyQt5 import uic, QtGui, QtCore, QtWidgets
 
 # Load the GUI class from the .ui file
-(Ui_MainWindow, QMainWindow) = uic.loadUiType('example.ui')
+(Ui_MainWindow, QMainWindow) = uic.loadUiType("example.ui")
 
 # Define a class for the main window.
-class MainWindow(QtGui.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         # Initialize the GUI itself
         super().__init__(parent)
@@ -20,41 +20,47 @@ class MainWindow(QtGui.QMainWindow):
     # Here, defining a method.
     # qt passes in additional arguments that this function does not need,
     # so I use *args to ignore them.
-    def from_SaveButtonClicked(self,*args):
-        filetype = '.set'
-        filename = str(QtGui.QFileDialog.getSaveFileName(self,'Save Settings File','',
-                                                         'Settings file (*{})'.format(filetype)))
-
+    def from_SaveButtonClicked(self, *args):
+        filetype = ".set"
+        filename, selected_type = QtWidgets.QFileDialog.getSaveFileName(
+            self, "Save Settings File", "", "Settings file (*{})".format(filetype)
+        )
         # filename is empty string if user hits cancel, so don't save anything
         if filename:
             if not filename.endswith(filetype):
                 filename += filetype
             self.SaveSettings(filename)
 
-    def SaveSettings(self,filename):
-        print('Saving',filename)
+    def SaveSettings(self, filename):
+        print("Saving", filename)
 
         # With statement automatically closes file at end.
-        with open(filename,'w') as f:
+        with open(filename, "w") as f:
             text_value = self.ui.text_entry.text()
-            f.write('Text field: {}\n'.format(text_value))
+            f.write("Text field: {}\n".format(text_value))
 
             integer_value = self.ui.spin_integer.value()
-            f.write('Integer field: {}\n'.format(integer_value))
+            f.write("Integer field: {}\n".format(integer_value))
 
             double_value = self.ui.spin_double.value()
-            f.write('Double field: {}\n'.format(double_value))
+            f.write("Double field: {}\n".format(double_value))
 
             dropdown_index = self.ui.dropdown.currentIndex()
             dropdown_text = self.ui.dropdown.currentText()
-            f.write('Dropdown: Index={}, Text={}\n'.format(dropdown_index,dropdown_text))
+            f.write(
+                "Dropdown: Index={}, Text={}\n".format(dropdown_index, dropdown_text)
+            )
 
 
-if __name__=='__main__':
+def main():
     # Initialize the qt event loop.
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     # Initialize and display our main window.
     w = MainWindow()
     w.show()
     # Run the qt event loop, exiting the script when done.
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
